2f0818932f0555b9e1e71b67bd4116c14395b50f
Seung-hwan285 <ghks285@naver.com>
2022-01-07 13:26:23 +0900

    refactor : 삭제하고 새로고침하면 데이터 없어지는 현상

--- todo.js
+++ todo.js
@@ -7,6 +7,8 @@
 
 // TODO 할일 목록 변경,삭제
 // -[x] 할일 목록 클릭하면 선긋기
+    // -[] toggle (할일 목록 선그어진거) localStorage로 관리하기
+
 // -[x] 삭제
     // -[x] 총개수 -1
     // -[x] 삭제 버튼누르면 안에 delete 버튼이 있는지 확인
@@ -22,8 +24,6 @@
     // -[x] set stringfy => 문자열 변환
     // -[x] get parse => json 객체 변환
     // -[x] delete 삭제
-
-
 
 
 const $=(s)=>document.querySelector(s);
@@ -134,8 +134,8 @@ if(getToDos !== null){
 
     // 로컬스토리지 문자열 값 -> 배열로 변환
     const parsedToDos = JSON.parse(getToDos);
-
-
+    // 남은 데이터 덮어주기 안그럼 새로고침했을때 값 초기화되는현상 나옴
+    list=parsedToDos;
     // 요소 하나하나 화면에 뿌려주기기
     parsedToDos.map(item=>{
 
